03c22040286caeccbe00fb08d0ff098cf3da9f77
Arpad <arpad@Arpads-MacBook-Pro.local>
2023-09-27 00:31:14 -0400

    update readme/App

--- README.old.md
+++ README.old.md
@@ -1,1 +1,9 @@
 # react-class-node-redux-ts
+
+## TODO
+
+1. add redux and typescript
+2. use flexbox challenges to add styling
+3. use string and array algorithm challenges to add functionality to the site -> games
+4. use testcafe to add integration testing
+5. use node/express for backend -> use simple db
--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
-class App extends Component() {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null,
+      data: "null",
     };
   }
   render() {
@@ -14,17 +14,7 @@ class App extends Component() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{JSON.stringify(this.state.data)}</p>
         </header>
       </div>
     );
